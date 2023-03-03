4ab39277006454a01e50d05ed0463c4a31df7835
Mahek Adhaduk <mahekadhaduk007@gmail.com>
2023-03-03 10:37:20 +0530

    initial commit

---
+++ src/components/CreditBox.jsx
@@ -0,0 +1,12 @@
+export const CreditBox = () => (
+  <address className="my-2 fs-6 text-white">
+    Created with <i className="text-danger fa fa-heart"></i> by
+    <a
+      href="https://github.com/makzpatel"
+      className="ms-2 text-reset text-decoration-underline"
+      rel="author"
+    >
+      Makz
+    </a>
+  </address>
+);
